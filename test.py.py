a5a7a901470c3411dca310805594c3ae1fc9d3c7
yujin202103683 <100991581+yujin202103683@users.noreply.github.com>
2022-03-06 22:09:58 +0900

    Add files via upload

---
+++ test.py.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# python test pile
+apple = 2000
+banana = 3000
+price = apple + banana
+
+print(apple, banana)
+print(apple, banana, sep=',')
+
+
+# In[12]:
+
+
+print(price)
+
+
+# In[8]:
+
+
+type(apple)
+
+
+# In[10]:
+
+
+type('apple')
+
+
+# In[ ]:
+
+
+
+
